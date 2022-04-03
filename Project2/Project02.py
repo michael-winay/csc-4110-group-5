5d9655775b2273b887cf61617ac5835db292ea69
Mike <mgwinay@gmail.com>
2022-04-03 19:44:32 -0400

    Update Project02.py
    -Created class "Drink"
    *accessors included

--- Project2/Project02.py
+++ Project2/Project02.py
@@ -5,6 +5,47 @@ import pickle
 from PIL import ImageTk, Image
 from os.path import exists
 
+"""Drink class"""
+
+class Drink:
+    def __init__(self, name, recipe, time, server, image):
+        self.name = name
+        self.recipe = recipe
+        self.time = time
+        self.server = server
+        self.image = ImageTk.PhotoImage(Image.open(image).resize((120, 246)))
+
+    def setName(self, name):
+        self.name = name
+
+    def setRecipe(self, recipe):
+        self.recipe = recipe
+
+    def setTime(self, time):
+        self.time = time
+
+    def setServer(self, server):
+        self.server = server
+
+    def setImage(self, image):
+        self.image = image
+
+    def getName(self):
+        return self.name
+
+    def getRecipe(self):
+        return self.recipe
+
+    def getTime(self):
+        return self.time
+
+    def getRecipe(self):
+        return self.server
+
+    def getImage(self):
+        return self.image
+
+
 database = {}
 file_exists = exists("database.pickle")
 if file_exists:
@@ -105,7 +146,7 @@ def delDrinkAction(id):
         print("Employee ID does not exist.")
         return 0"""
 
-def findDrink(drinkImage):
+def findDrink(drink):
 
     """ Creates a query button."""
 
@@ -126,8 +167,8 @@ def findDrink(drinkImage):
     findForm.title("Order Drink from the Menu")
     findForm.geometry('500x400')
     
-    findDrinkLabel = tk.Label(findForm, image=drinkImage)
-    findDrinkLabel.grid(row = 0,column = 1)
+    findDrinkLabel = tk.Label(findForm, image=drink.getImage())
+    findDrinkLabel.grid(row = 0,column = 0)
 
 #Back end function
 def findDrinkAction(id):
@@ -240,17 +281,57 @@ def quit():
     root.destroy()
 
 if __name__ == '__main__':
+
     root = tk.Tk()
     root.title("Main View")
     root.geometry('800x246')
     #root.configure(background = "grey");
 
-    menuImageOne = ImageTk.PhotoImage(Image.open('images/old-fashioned.png').resize((120, 246)))
-    menuImageTwo = ImageTk.PhotoImage(Image.open('images/negroni.png').resize((120, 246)))
-    menuImageThree = ImageTk.PhotoImage(Image.open('images/moscow-mule.png').resize((120, 246)))
-    menuImageFour = ImageTk.PhotoImage(Image.open('images/martini.png').resize((120, 246)))
-    menuImageFive = ImageTk.PhotoImage(Image.open('images/margarita.png').resize((120, 246)))
-    menuImageSix = ImageTk.PhotoImage(Image.open('images/cosmopolitan.png').resize((120, 246)))
+    oldfashioned = Drink("old-fashioned",\
+        ["- 2 oz bourbon or rye whiskey",\
+        "- 2 dashes Angostura bitters",\
+        "- 1 sugar cube or 1 tsp sugar"\
+        "- Orange twist garnish"],\
+        5, "Michael", 'images/old-fashioned.png')
+
+    negroni = Drink("negroni",\
+        ["- 1 oz gin",\
+        "- 1 oz Campari",\
+        "- 1 oz sweet vermouth"],\
+        3, "Michael", 'images/negroni.png')
+
+    moscowmule = Drink("moscow-mule",\
+        ["- 2 oz vodka",\
+        "- 4 to 6 oz ginger beer",\
+        "- .5 oz lime juice"],\
+        3, "Michael", 'images/moscow-mule.png')
+
+    martini = Drink("martini",\
+        ["- 3 oz gin or vodka",\
+        "- .5 oz dry vermouth",\
+        "- Lemon peel or olive"],\
+        5, "Michael", 'images/old-fashioned.png')
+
+    margarita = Drink("margarita",\
+        ["- 2 oz silver tequila"
+        "- 1 oz Cointreau",\
+        "- 1 oz lime juice",\
+        "- Salt for the rim"],\
+        5, "Michael", 'images/margarita.png')
+
+    cosmopolitan = Drink("cosmopolitan",\
+        ["- 1.5 oz citrus vodka",\
+        "- 1 oz Cointreau",\
+        "- .5 oz lime juice",\
+        "- .25 oz cranberry juice"],\
+        5, "Michael", 'images/cosmopolitan.png')
+
+    #menuImageOne = ImageTk.PhotoImage(Image.open('images/old-fashioned.png').resize((120, 246)))
+    #menuImageTwo = ImageTk.PhotoImage(Image.open('images/negroni.png').resize((120, 246)))
+    #menuImageThree = ImageTk.PhotoImage(Image.open('images/moscow-mule.png').resize((120, 246)))
+    #menuImageFour = ImageTk.PhotoImage(Image.open('images/martini.png').resize((120, 246)))
+    #menuImageFive = ImageTk.PhotoImage(Image.open('images/margarita.png').resize((120, 246)))
+    #menuImageSix = ImageTk.PhotoImage(Image.open('images/cosmopolitan.png').resize((120, 246)))
 
     """menuLabelOne = tk.Label(image=menuImageOne)
     menuLabelTwo = tk.Label(image=menuImageTwo)
@@ -258,24 +339,26 @@ if __name__ == '__main__':
     menuLabelFour = tk.Label(image=menuImageFour)
     menuLabelFive = tk.Label(image=menuImageFive)
     menuLabelSix = tk.Label(image=menuImageSix)"""
-
-    menuButtonOne = ttk.Button(root, image=menuImageOne, command = lambda:findDrink(menuImageOne))
+    
+    menuButtonOne = ttk.Button(root, image=oldfashioned.getImage(), command = lambda:findDrink(oldfashioned))
     menuButtonOne.grid(row = 0,column = 0)
 
-    menuButtonTwo = ttk.Button(root, image=menuImageTwo, command = lambda:findDrink(menuImageTwo))
+    menuButtonTwo = ttk.Button(root, image=negroni.getImage(), command = lambda:findDrink(negroni))
     menuButtonTwo.grid(row = 0,column = 1)
 
-    menuButtonThree = ttk.Button(root, image=menuImageThree, command = lambda:findDrink(menuImageThree))
+    menuButtonThree = ttk.Button(root, image=moscowmule.getImage(), command = lambda:findDrink(moscowmule))
     menuButtonThree.grid(row = 0,column = 2)
 
-    menuButtonFour = ttk.Button(root, image=menuImageFour, command = lambda:findDrink(menuImageFour))
+    menuButtonFour = ttk.Button(root, image=martini.getImage(), command = lambda:findDrink(martini))
     menuButtonFour.grid(row = 0,column = 3)
 
-    menuButtonFive = ttk.Button(root, image=menuImageFive, command = lambda:findDrink(menuImageFive))
+    menuButtonFive = ttk.Button(root, image=margarita.getImage(), command = lambda:findDrink(margarita))
     menuButtonFive.grid(row = 0,column = 4)
 
-    menuButtonSix = ttk.Button(root, image=menuImageSix, command = lambda:findDrink(menuImageSix))
+    menuButtonSix = ttk.Button(root, image=cosmopolitan.getImage(), command = lambda:findDrink(cosmopolitan))
     menuButtonSix.grid(row = 0,column = 5)
+
+    menuButtonDelete = ttk.Button(root, text="Delete Drink")
 
     menuButtonQuit = ttk.Button(root, text="Quit", command = quit)
     menuButtonQuit.grid(row = 1, column = 0)
